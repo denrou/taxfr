6a6f7013c0726ed3665e90a36ae712e727ab3fb4
Denis Roussel <deroussel@gmail.com>
2019-02-20 17:01:17 +0100

    Update data format

--- data-raw/tax.R
+++ data-raw/tax.R
@@ -8,10 +8,14 @@ library(tidyr)
 
 # Data downloading ----
 
-dowload_and_extract <- function(url) {
+download_and_extract <- function(url) {
   destfile <- tempfile(fileext = ".zip")
-  download.file(url, destfile)
-  unzip(destfile, exdir = "data-raw")
+  cat(glue("Trying {url}"))
+  try(download.file(url, destfile, quiet = TRUE), silent = TRUE)
+  if (file.exists(destfile)) {
+    unzip(destfile, exdir = "data-raw")
+    file.remove(destfile)
+  }
 }
 today_year  <- as.integer(strftime(Sys.Date(), "%Y")) - 1L
 years       <- seq(2011, today_year, by = 1L)
@@ -21,7 +25,7 @@ years       <- seq(2011, today_year, by = 1L)
 sep         <- c(rep("", 5), rep("_", length(years) - 5))
 files       <- glue("ircom_{years}_revenus{sep}{years - 1L}.zip")
 urls        <- glue("https://www.impots.gouv.fr/portail/www2/fichiers/statistiques/base_de_donnees/ircom/{files}")
-walk(urls, dowload_and_extract)
+walk(urls, download_and_extract)
 
 # Between 2011 and 2016, files are exported in xls format which is not well imported with readxl package.
 # From 2017, files are exported in xlsx format
@@ -57,19 +61,19 @@ paths <- list.files("data-raw", pattern = ".xlsx", full.names = TRUE)
 nas       <- c(rep("n.d.", 3), rep("n.c.", length(paths) - 3))
 pb1       <- progress_estimated(length(paths)) # always nice to have a progress bar ;)
 col_names <- c(
-  "code_departement",
-  "code_commune",
-  "commune",
-  "revenu_fiscal_de_reference_par_tranche_en_euros",
-  "nombre_de_foyers_fiscaux",
-  "revenu_fiscal_de_reference_des_foyers_fiscaux",
-  "impot_net_total",
-  "nombre_de_foyers_fiscaux_imposes",
-  "revenu_fiscal_de_reference_des_foyers_fiscaux_imposes",
-  "nombre_de_foyers_concernes_retraites_et_pensions",
-  "montant_retraites_et_pensions",
-  "nombre_de_foyers_concernes_traitements_et_salaires",
-  "montant_traitements_et_salaires"
+  "department_code",
+  "municipality_code",
+  "municipality",
+  "reference_tax_revenue_per_tranche_in_euros",
+  "number_of_households",
+  "revenue_of_households",
+  "tax_income",
+  "number_of_taxable_households",
+  "revenue_of_taxable_households",
+  "number_of_households_concerned_by_pensions",
+  "revenue_gain_by_pensions",
+  "number_of_households_concerned_by_salaries",
+  "revenue_gain_by_salaries"
 )
 tax_raw   <- map2_dfr(paths, nas, function(path, nas) {
   pb1$tick()$print()
@@ -80,8 +84,7 @@ tax_raw   <- map2_dfr(paths, nas, function(path, nas) {
   tax_year <- map_dfr(sheets, read_excel, path = path,  skip = 4, na = nas, col_names = col_names, progress = FALSE)
   # Since all excel files will be gathered together, we need to add year information
   tax_year %>%
-    mutate(annee = year, annee_revenu = year - 1) %>%
-    clean_names()
+    mutate(year = year)
 
 })
 pb1$stop()
@@ -90,46 +93,24 @@ pb1$stop()
 
 # Data tidyfication ----
 
-# Data does not contain only one tax information, and it also contains data on number of people concerned and how much
-# tax have been collected.
-# For the data to be tidy, some homogeneisations are done using the following tibble.
-# Later, this tibble will be join with raw data.
-ref <- tribble(
-  ~type                      , ~nombre                                             , ~revenu,
-  "Foyers fiscaux"           , "nombre_de_foyers_fiscaux"                          , "revenu_fiscal_de_reference_des_foyers_fiscaux",
-  "Foyers fiscaux imposés"   , "nombre_de_foyers_fiscaux_imposes"                  , "revenu_fiscal_de_reference_des_foyers_fiscaux_imposes",
-  "Traitements et salaires"  , "nombre_de_foyers_concernes_traitements_et_salaires", "montant_traitements_et_salaires",
-  "Retraites et pensions"    , "nombre_de_foyers_concernes_retraites_et_pensions"  , "montant_retraites_et_pensions"
-) %>%
-  gather(what, val, -type) %>%
-  mutate_at("type", factor, ordered = TRUE)
-
 # Start cleaning data
 tax <- tax_raw %>%
-  mutate_at("commune", tolower) %>%
+  mutate_at("municipality", str_to_title) %>%
   # Convert to factors to have the right order
-  mutate_at("revenu_fiscal_de_reference_par_tranche_en_euros", factor,
-            levels = c("0 à 10 000", "10 001 à 12 000", "12 001 à 15 000", "15 001 à 20 000", "20 001 à 30 000",
-                       "30 001 à 50 000", "50 001 à 100 000", "+ de 100 000", "Total"), ordered = TRUE) %>%
+  mutate_at("reference_tax_revenue_per_tranche_in_euros", str_replace, pattern = "à", replacement = "to") %>%
+  mutate_at("reference_tax_revenue_per_tranche_in_euros", factor,
+            levels = c("0 to 10 000", "10 001 to 12 000", "12 001 to 15 000", "15 001 to 20 000", "20 001 to 30 000",
+                       "30 001 to 50 000", "50 001 to 100 000", "+ de 100 000", "Total"), ordered = TRUE) %>%
   # The column `revenu_fiscal_de_reference_par_tranche_en_euros`, is a column that must be filled.
   # We use it to remove empty lines and non necessary data
-  filter(!is.na(revenu_fiscal_de_reference_par_tranche_en_euros)) %>%
+  filter(!is.na(reference_tax_revenue_per_tranche_in_euros)) %>%
   # Starting from 2016, taxes are given per thousands of euros instead of euros
-  mutate_at(c("nombre_de_foyers_fiscaux", "revenu_fiscal_de_reference_des_foyers_fiscaux", "impot_net_total"), as.numeric) %>%
-  mutate_at("revenu_fiscal_de_reference_des_foyers_fiscaux", funs(if_else(.data$annee %in% c(2016, 2017), . * 1000, .))) %>%
-  # Regroup tax under a common column using the reference table defined earlier
-  gather(col, val,
-         starts_with("nombre"), c("revenu_fiscal_de_reference_des_foyers_fiscaux",
-                                  "montant_traitements_et_salaires", "montant_retraites_et_pensions",
-                                  "revenu_fiscal_de_reference_des_foyers_fiscaux_imposes")) %>%
-  mutate_at("val", as.numeric) %>%
-  left_join(ref, by = c("col" = "val")) %>%
-  select(-col) %>%
-  spread(what, val) %>%
+  mutate_at(vars(starts_with("number"), starts_with("revenue"), "tax_income"), as.numeric) %>%
+  mutate_at("revenue_of_taxable_households", list(~if_else(.data$year %in% c(2016, 2017), . * 1000, .))) %>%
   # Get INSEE code
-  mutate_at("code_departement", str_sub, end = 2L) %>%
-  unite("code_insee", code_departement, code_commune, sep = "", remove = FALSE) %>%
-  mutate(code_insee = if_else(str_detect(code_insee, "NA"), NA_character_, code_insee))
+  mutate(postal_code = paste0(str_sub(department_code, end = 2L), municipality_code)) %>%
+  mutate_at("postal_code", list(~if_else(str_detect(., "NA"), NA_character_, .))) %>%
+  select(department_code, municipality_code, postal_code, municipality, everything())
 
 # Data saving ----
 usethis::use_data(tax, overwrite = TRUE)
